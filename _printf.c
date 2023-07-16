85777762025cf8ac8b9f5018e72789672f53228b
root <Joshcones1253@yahoo.com>
2023-07-16 10:28:13 -0700

    COWBOYS

--- _printf.c
+++ _printf.c
@@ -2,6 +2,7 @@
 #include <limits.h>
 #include <stdio.h>
 int checkFormat(char tempc, va_list list);
+int print_char2(char c);
 /**
  * _printf - Prints the values given with a separator
  * @format: The value to be printed
